baeea688087bc0d86070d7ecf8ed20d2b8923388
nurlitadf <nurlitadf17@gmail.com>
2020-05-01 21:26:56 +0700

    delete register user

--- api/models/user.js
+++ api/models/user.js
@@ -6,38 +6,6 @@ const saltRounds = 10;
 let response = null;
 let qb = null;
 
-exports.register = async (username, password) => {
-    try {
-        const settings = {
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-        };
-        const pool = new QueryBuilder(settings, 'mysql', 'pool');
-
-        qb = await pool.get_connection();
-
-        await bcrypt.hash(password, saltRounds, async (err, hashedPassword) => {
-            if (err) {
-              return err;
-            }
-            else {
-                console.log(username, password);
-
-                response = await qb.returning('id').insert('user', {username: username, password: hashedPassword});
-         
-                console.log("Query Ran: " + qb.last_query());
-                qb.disconnect();
-                
-                return response;
-            }
-        });
-    } catch (err) {
-        return console.error("Uh oh! Couldn't get results: " + err.msg);
-    }
-}
-
 exports.login = async (username, password) => {
     try {
         const settings = {
